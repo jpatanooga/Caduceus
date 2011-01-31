6fc444f87daf0eaaaffd50a74f3b4b96a146849b
Josh Patterson <jpatterson@floe.tv>
2011-01-31 01:01:34 -0600

    updated gzip sequence file example

--- src/tv/floe/caduceus/hadoop/compression/lzo/create_gzip_seq_file/CreateSeqFileJob.java
+++ src/tv/floe/caduceus/hadoop/compression/lzo/create_gzip_seq_file/CreateSeqFileJob.java
@@ -1,11 +1,154 @@
 package tv.floe.caduceus.hadoop.compression.lzo.create_gzip_seq_file;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+
 /**
  * Simple job that takes LZO input text and creates gzipped sequence files
  * 
  * @author jpatterson
  *
  */
-public class CreateSeqFileJob {
+public class CreateSeqFileJob extends Configured implements Tool
+{
 
-}
+	   private static final Logger logger = Logger.getLogger(CreateSeqFileJob.class);
+
+	   @Override
+	   public int run(String[] args) throws Exception {
+
+		   System.out.println( "\n\nRunning Create GZIP'd SequenceFile Example Job\n" );
+		   
+		   
+		   JobConf conf = new JobConf( getConf(), CreateSeqFileJob.class );
+		   conf.setJobName( "CreateSeqFileJob" );
+		   
+		   conf.setMapOutputKeyClass( Text.class );
+		   conf.setMapOutputValueClass( Text.class );
+
+		//   conf.setMapperClass( ReadLZOFileMapper.class );        
+		//   conf.setReducerClass( ReadLZOFileReducer.class );
+		 
+	   
+		   boolean bLZO = false;
+		   boolean bCompressOutput = false;
+		   
+		   List<String> other_args = new ArrayList<String>();
+		   for(int i=0; i < args.length; ++i) {
+		     try {
+		       if ("-m".equals(args[i])) {
+		       	
+		         conf.setNumMapTasks(Integer.parseInt(args[++i]));
+		         
+		       } else if ("-r".equals(args[i])) {
+		       	
+		         conf.setNumReduceTasks(Integer.parseInt(args[++i]));
+			       	
+			       	
+		       } else if ("-lzo_input".equals(args[i])) {
+		         
+		    	   bLZO = true;
+		    	   
+		       } else if ( "-lzo_output".equals( args[i] ) ) {
+		    	   
+		    	   bCompressOutput = true;
+		    	   
+		       } else {
+		       	
+		         other_args.add(args[i]);
+		         
+		       }
+		     } catch (NumberFormatException except) {
+		       System.out.println("ERROR: Integer expected instead of " + args[i]);
+		       return printUsage();
+		     } catch (ArrayIndexOutOfBoundsException except) {
+		       System.out.println("ERROR: Required parameter missing from " +
+		                          args[i-1]);
+		       return printUsage();
+		     }
+		   }
+		   // Make sure there are exactly 2 parameters left.
+		   if (other_args.size() != 2) {
+		     System.out.println("ERROR: Wrong number of parameters: " +
+		                        other_args.size() + " instead of 2.");
+		     return printUsage();
+		   }
+		   
+		   if (bLZO) {
+			   
+			   System.out.println( "\n\nUsing LzoTextInputFormat\n" );
+			   
+			   // when we switch over to LZO compressed formats, will need hadoop-lzo.jar in path
+			   conf.setInputFormat( DeprecatedLzoTextInputFormat.class );
+			   
+		   } else {
+			   
+			   System.out.println( "Using TextInputFormat" );
+
+			   // for basic non-compressed formats:
+			   conf.setInputFormat( TextInputFormat.class );
+			   
+		   }
+		   
+		   conf.setOutputFormat(SequenceFileOutputFormat.class);
+		   conf.setCompressMapOutput(true);	   
+		   
+		   if (bCompressOutput) {
+			   
+			   System.out.println( "Using LZO To Compress the job Output" );
+			   
+			   TextOutputFormat.setOutputCompressorClass(conf, org.apache.hadoop.io.compress.GzipCodec.class);
+			   TextOutputFormat.setCompressOutput(conf, true);
+
+		   } else {
+			   
+			   System.out.println( "No compression on job Output" );
+			   
+		   }
+		   
+		  		   
+		   FileInputFormat.setInputPaths( conf, other_args.get(0) );
+		   FileOutputFormat.setOutputPath( conf, new Path(other_args.get(1)) );
+		   
+		   
+		   
+		   JobClient.runJob(conf);
+		   
+		   return 0;
+	   }
+	   
+		 
+		
+		 static int printUsage() {
+		   System.out.println("CreateSeqFileJob [-m <maps>] [-r <reduces>] <input> <output>");
+		   ToolRunner.printGenericCommandUsage(System.out);
+		   return -1;
+		 }
+
+	   
+		 public static void main(String[] args) throws Exception {
+		   
+			int res = ToolRunner.run( new Configuration(), new CreateSeqFileJob(), args );
+		    System.exit(res);
+		 
+		 }
+	   
+	   
+	}
+
